0f25569809b96f7d225d85bba3f239c871824e7d
Mitchell <munachisonjeze@gmail.com>
2026-07-20 20:41:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+/*=====================================================
+                MOBILE NAVIGATION
+=====================================================*/
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("JavaScript Loaded");
+
+    const hamburger = document.querySelector(".hamburger");
+    const mobileMenu = document.querySelector(".mobile-menu");
+    const mobileLinks = document.querySelectorAll(".mobile-menu a");
+
+    if (!hamburger || !mobileMenu) {
+        console.error("Hamburger or mobile menu not found.");
+        return;
+    }
+
+    // Toggle menu
+    hamburger.addEventListener("click", function (e) {
+
+        e.stopPropagation();
+
+        hamburger.classList.toggle("active");
+        mobileMenu.classList.toggle("active");
+
+        document.body.classList.toggle("menu-open");
+
+    });
+
+    // Close when clicking a link
+    mobileLinks.forEach(link => {
+
+        link.addEventListener("click", () => {
+
+            hamburger.classList.remove("active");
+            mobileMenu.classList.remove("active");
+            document.body.classList.remove("menu-open");
+
+        });
+
+    });
+
+    // Close when clicking outside
+    document.addEventListener("click", function (e) {
+
+        if (
+            !mobileMenu.contains(e.target) &&
+            !hamburger.contains(e.target)
+        ) {
+
+            hamburger.classList.remove("active");
+            mobileMenu.classList.remove("active");
+            document.body.classList.remove("menu-open");
+
+        }
+
+    });
+
+});
+/*==================================================
+        SERVICE POPUP
+==================================================*/
+
+const modal = document.getElementById("serviceModal");
+
+const modalImage = document.getElementById("modalImage");
+
+const modalTitle = document.getElementById("modalTitle");
+
+const modalDescription = document.getElementById("modalDescription");
+
+const learnButtons = document.querySelectorAll(".learn-more");
